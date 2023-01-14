ec58130bb0f777b26d18a540180296f659ee60b5
Roger Augusto <56980703+rgr14@users.noreply.github.com>
2023-01-13 22:43:50 -0300

    Criando Entity e Migrations

---
+++ migrations/Version20230113131946.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230113131946 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE usuario DROP senha, CHANGE nome nome VARCHAR(100) NOT NULL, CHANGE email email VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE usuario ADD senha VARCHAR(256) NOT NULL, CHANGE nome nome VARCHAR(64) NOT NULL, CHANGE email email VARCHAR(64) NOT NULL');
+    }
+}
---
+++ src/Entity/Usuario.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsuarioRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UsuarioRepository::class)
+ */
+class Usuario
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $nome;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $data_cadastro;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getDataCadastro(): ?\DateTimeInterface
+    {
+        return $this->data_cadastro;
+    }
+
+    public function setDataCadastro(\DateTimeInterface $data_cadastro): self
+    {
+        $this->data_cadastro = $data_cadastro;
+
+        return $this;
+    }
+}

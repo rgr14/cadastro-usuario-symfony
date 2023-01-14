546209fe715710b07e316642c3574c3599471f67
Roger Augusto <56980703+rgr14@users.noreply.github.com>
2023-01-13 22:43:37 -0300

    Finalizando UsuarioController Web

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -2,15 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Usuario;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @Route("/", name="web_usuario_")
  */
 class UsuarioController extends AbstractController
 {
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     /**
      * @Route("/",methods={"GET"}, name="index")
      */
@@ -22,8 +32,26 @@ class UsuarioController extends AbstractController
     /**
      * @Route("/salvar",methods={"POST"}, name="salvar")
      */
-    public function salvar()
+    public function salvar(Request $request)
     {
-        return new Response("implementar", 200);
+        $data = $request->request->all();
+        
+        $usuario = new Usuario();
+        $usuario->setNome($data['nome']);
+        $usuario->setEmail($data['email']);
+        $usuario->setDataCadastro(new \DateTime());
+        
+        $orm = $this->doctrine->getManager();
+        $orm->persist($usuario);
+        $orm->flush();
+
+        if($orm->contains($usuario))
+        {
+            return $this->render("usuario/sucesso.html.twig");
+        }
+        else
+        {
+            return $this->render("usuario/erro.html.twig");
+        }
     }
 }
